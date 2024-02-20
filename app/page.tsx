a8fee13ebebf499043b02f7dff551df0d257fc67
NourAddine <83648734+NourEldinAk@users.noreply.github.com>
2024-02-20 23:24:39 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+import Content from "../components/Content";
 
 export default function Home() {
   return (
---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const NavLinks = [
+    {href:'/', key:'home', text:'Home'},
+    {href:'/menu', key:'menu', text:'Menu'},
+    {href:'/reserve', key:'reserve', text:'reserve'},
+    {href:'/cart', key:'cart', text:'Cart'},
+    {href:'/contact', key:'contact', text:'contact'},
+]
