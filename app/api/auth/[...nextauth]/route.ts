6e8a002521689295c0bd90509658ae1079c7a02f
NourAddine <83648734+NourEldinAk@users.noreply.github.com>
2024-03-06 18:01:21 +0300

    fixed next-auth and updated prisma models

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import { AuthOptions } from "@/utils/AuthOptions";
 import NextAuth from "next-auth/next";
 
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 const handler = NextAuth(AuthOptions)
 
--- utils/AuthOptions.tsx
+++ utils/AuthOptions.tsx
@@ -1,15 +1,17 @@
 import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "./connect";
 
 export const AuthOptions: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-      ],
-      adapter: PrismaAdapter
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
 }
 
 export const getAuthSession = ()=> getServerSession(AuthOptions);
